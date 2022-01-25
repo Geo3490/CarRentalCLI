5497ea8c9a0afb1c0705ee476f492d8be2f283c7
Geo3490 <geo40127@gmail.com>
2022-01-24 19:25:25 -0500

    update

---
+++ src/com/careerdevs/Car.java
@@ -0,0 +1,13 @@
+package com.careerdevs;
+
+public class Car {
+    public String make;
+    public String model;
+    public boolean isRented;
+
+    public Car(String make, String model, boolean isRented){
+
+
+
+    }
+}
---
+++ src/com/careerdevs/RentalService.java
@@ -0,0 +1,10 @@
+package com.careerdevs;
+
+public class RentalService {
+
+    public static void main(String[] args) {
+        String welcome = "Welcome to the com.careerdevs.Car Rental CLI";
+
+        System.out.println(welcome);
+    }
+}
