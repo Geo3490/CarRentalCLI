804c7595f50fdb76d9d1abb33f07f41cac6ad12b
Geo3490 <geo40127@gmail.com>
2022-02-10 20:54:18 -0500

    updated2

--- src/com/careerdevs/RentalService.java
+++ src/com/careerdevs/RentalService.java
@@ -12,14 +12,29 @@ public class RentalService {
 //int testInt = UI.readInt("Enter a num", 1, 10);
 //
 //        System.out.println(testInt);
+    initializeCarStorage();
+    carStorage.get(1).setRented(true);//FOR TESTING
+        rentalMenu();
+    }
 
+    private static void rentalMenu(){
+        System.out.println("\nAVAILABLE CARS\n");
+        int listNumber = 1;
+        for (int i = 0; i < carStorage.size(); i++){
+            if (!carStorage.get(i).isRented()){
+                System.out.println(listNumber + ") " + carStorage.get(i).getName());
+                listNumber++;
+            }
+        }
     }
 
     private static void initializeCarStorage(){
         carStorage = new ArrayList<>();
 
         Car car1 = new Car("Honda", "Accord",false);
+
         Car car2 = new Car("Chevy", "Tahoe",false);
+
         Car car3 = new Car("Ford", "F150",false);
 
         carStorage.addAll(List.of(new Car[]{car1, car2, car3}));
